5f3096d6d15e371da6244f01d1b5200aa9e71c71
miyanokomiya <tomoya.hstexze24632@gmail.com>
2018-12-08 16:17:23 +0900

    ユーザー作成を認証不要にする

--- app/server/route.go
+++ app/server/route.go
@@ -11,14 +11,16 @@ func RouteV1(app *gin.Engine) {
 	authHandler := handlers.NewAuthHandler()
 	usersHandler := handlers.NewUsersHandler()
 
+	app.Use(middlewares.CORSMiddleware())
 	app.POST("/login", authHandler.Login)
 	app.DELETE("/logout", authHandler.Logout)
+
+	app.POST("/users", usersHandler.Create)
 
 	private := app.Group("/private")
 	private.Use(middlewares.AuthRequired())
 	{
 		private.GET("/users", usersHandler.Index)
-		private.POST("/users", usersHandler.Create)
 		private.GET("/users/:id", usersHandler.Show)
 		private.PATCH("/users/:id", usersHandler.Update)
 		private.DELETE("/users/:id", usersHandler.Delete)
