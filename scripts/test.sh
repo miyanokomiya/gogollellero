5f06cd730cb2789121ad854a3d99ca597ae95b81
miyanokomiya <tomoya.hstexze24632@gmail.com>
2018-12-12 10:17:50 +0900

    関数単位テスト対応

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,8 @@
 docker exec -it gogollellero_web goose -env test up
 if [ $# -eq 0 ]; then
   docker exec -it gogollellero_web bash -c "GO_ENV=test go test github.com/miyanokomiya/gogollellero/app/server/..."
-else
+elif [ $# -eq 1 ]; then
   docker exec -it gogollellero_web bash -c "GO_ENV=test go test github.com/miyanokomiya/gogollellero/app/server/$1/..."
+else
+  docker exec -it gogollellero_web bash -c "GO_ENV=test go test github.com/miyanokomiya/gogollellero/app/server/$1/... -run $2"
 fi
