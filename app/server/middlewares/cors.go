a5d8830ca901d064e50ec9bc66138eab88553372
miyanokomiya <tomoya.hstexze24632@gmail.com>
2018-12-14 08:12:21 +0900

    corsのorigin取得方法を変更

--- app/server/middlewares/cors.go
+++ app/server/middlewares/cors.go
@@ -1,15 +1,15 @@
 package middlewares
 
 import (
-	"os"
-
 	"github.com/gin-gonic/gin"
 )
 
 // CORSMiddleware cors対応
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", os.Getenv("CLIENT_HOST"))
+		for _, origin := range c.Request.Header["Origin"] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
