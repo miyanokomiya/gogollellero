b29bdf9bf038398219b17c2b00cebc5f257b342e
miyanokomiya <tomoya.hstexze24632@gmail.com>
2018-12-14 08:41:35 +0900

    アセットバンドルやり直し

--- app/server/assets/configs.go
+++ app/server/assets/configs.go
@@ -1,1 +1,28 @@
-OCI runtime exec failed: exec failed: container_linux.go:348: starting container process caused "exec: \"go-assets-builder\": executable file not found in $PATH": unknown
+package assets
+
+import (
+	"time"
+
+	"github.com/jessevdk/go-assets"
+)
+
+var _Configsec27b42f4e737cebcd9245ac050bb73926b63135 = "driver: mysql\nprotocol: tcp(db:3306)\ndevelopment:\n  user: miyanokomiya\n  password: miyanokomiya\n  db: gogollellero\ntest:\n  user: miyanokomiya\n  password: miyanokomiya\n  db: gogollellero_test\n"
+
+// Configs returns go-assets FileSystem
+var Configs = assets.NewFileSystem(map[string][]string{"/": []string{"configs"}, "/configs": []string{"db.yml"}}, map[string]*assets.File{
+	"/": &assets.File{
+		Path:     "/",
+		FileMode: 0x800001ed,
+		Mtime:    time.Unix(1544743800, 1544743800000000000),
+		Data:     nil,
+	}, "/configs": &assets.File{
+		Path:     "/configs",
+		FileMode: 0x800001ed,
+		Mtime:    time.Unix(1543749886, 1543749886000000000),
+		Data:     nil,
+	}, "/configs/db.yml": &assets.File{
+		Path:     "/configs/db.yml",
+		FileMode: 0x1a4,
+		Mtime:    time.Unix(1544743777, 1544743777000000000),
+		Data:     []byte(_Configsec27b42f4e737cebcd9245ac050bb73926b63135),
+	}}, "")
